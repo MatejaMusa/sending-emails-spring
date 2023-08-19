1e0fe7954cd06db560b78a89241184b7f5f9ec1a
MatejaMusa <matejamusa6@gmail.com>
2023-08-19 06:39:49 +0200

    add EmailService and method to send regular emails

---
+++ src/main/java/com/personalproject/userservice/service/EmailService.java
@@ -0,0 +1,10 @@
+package com.personalproject.userservice.service;
+
+public interface EmailService {
+    void sendSimpleMailMessage(String name, String to, String token);
+    void sendMimeMessageWithAttachments(String name, String to, String token);
+    void sendMimeMessageWithEmbeddedImages(String name, String to, String token);
+    void sendMimeMessageWithEmbeddedFiles(String name, String to, String token);
+    void sendHtmlEmail(String name, String to, String token);
+    void sendHtmlEmailWithEmbeddedFiles(String name, String to, String token);
+}
---
+++ src/main/java/com/personalproject/userservice/service/impl/EmailServiceImpl.java
@@ -0,0 +1,59 @@
+package com.personalproject.userservice.service.impl;
+
+import com.personalproject.userservice.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    @Value("${spring.mail.verify.host}")
+    private String host;
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+    private final JavaMailSender emailSender;
+
+    @Override
+    public void sendSimpleMailMessage(String name, String to, String token) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setSubject("New User Account Verification");
+            message.setFrom(fromEmail);
+            message.setTo(to);
+            message.setText("Verify your email");
+            emailSender.send(message);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void sendMimeMessageWithAttachments(String name, String to, String token) {
+
+    }
+
+    @Override
+    public void sendMimeMessageWithEmbeddedImages(String name, String to, String token) {
+
+    }
+
+    @Override
+    public void sendMimeMessageWithEmbeddedFiles(String name, String to, String token) {
+
+    }
+
+    @Override
+    public void sendHtmlEmail(String name, String to, String token) {
+
+    }
+
+    @Override
+    public void sendHtmlEmailWithEmbeddedFiles(String name, String to, String token) {
+
+    }
+}
